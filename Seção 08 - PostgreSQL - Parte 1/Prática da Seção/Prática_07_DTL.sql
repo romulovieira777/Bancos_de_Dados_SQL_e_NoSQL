3f85a7a9a89755d8f6116f27eb83bbc14d76eda9
romulovieira777 <romulo.vieira777@gmail.com>
2021-07-27 17:55:07 -0300

    PostgreSQL - Parte 1

--- Seção 08 - PostgreSQL - Parte 1/Prática da Seção/Prática_07_DTL.sql
+++ Seção 08 - PostgreSQL - Parte 1/Prática da Seção/Prática_07_DTL.sql
@@ -1,1 +1,41 @@
 -- Iniciando uma Transação
+BEGIN TRANSACTION;
+
+-- Inserindo Valores na Tabela Tipos Produto
+INSERT INTO tipos_produto
+(
+    descricao
+) VALUES (
+    'Equipamentos'
+);
+
+INSERT INTO tipos_produto
+(
+    descricao
+) VALUES (
+    'Nobreak'
+);
+
+-- Comitando a Transação e Encerrando
+COMMIT;
+
+-- Iniciando uma Transação
+BEGIN TRANSACTION;
+
+-- Inserindo Dados na Tabela Tipos Produto
+INSERT INTO tipos_produto
+(
+    descricao
+) VALUES (
+    'Tipo A'
+);
+
+INSERT INTO tipos_produto
+(
+    descricao
+) VALUES (
+    'Tipo B'
+);
+
+-- Desfazer a Última Transação
+ROLLBACK;
