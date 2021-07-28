7933c77d56487fecee0909fe9be0c7336292ca94
romulovieira777 <romulo.vieira777@gmail.com>
2021-07-28 14:31:37 -0300

    PostgreSQL - Parte 1

---
+++ Seção 08 - PostgreSQL - Parte 1/Prática da Seção/Prática_08.sql
@@ -0,0 +1,79 @@
+-- Criando Banco de Dados secao_05
+CREATE DATABASE IF NOT EXISTS
+    secao_05;
+
+-- Criando Tabela Tipos Produto
+CREATE TABLE IF NOT EXISTS tipos_produto
+(
+    codigo SERIAL PRIMARY KEY
+  , descricao VARCHAR(30) NOT NULL 
+);
+
+-- Criando Tabela Produtos
+CREATE TABLE IF NOT EXISTS produtos
+(
+    codigo SERIAL PRIMARY KEY
+  , descricao VARCHAR(30) NOT NULL
+  , preco MONEY NOT NULL
+  , codigo_tipo INT REFERENCES tipos_produto(codigo) NOT NULL
+);
+
+-- Inserindo Dados na Tabela Tipos Produto
+INSERT INTO tipos_produto
+(
+    descricao
+) VALUES (
+    'Computadores'
+);
+
+INSERT INTO tipos_produto
+(
+    descricao
+) VALUES (
+    'Impressoras'
+);
+
+-- Inserindo Dados na Tabela Produtos
+INSERT INTO produtos
+(
+    descricao
+  , preco
+  , id_tipo_produto
+) VALUES (
+    'Desktop'
+   , 1200
+   , 1
+);
+
+INSERT INTO produtos
+(
+    descricao
+  , preco
+  , id_tipo_produto
+) VALUES (
+    'Laptop'
+   , 1800
+   , 1
+);
+
+INSERT INTO produtos
+(
+    descricao
+  , preco
+  , id_tipo_produto
+) VALUES (
+    'Impr. Jato Tinta'
+   , 300
+   , 2
+);
+
+INSERT INTO produtos
+(
+    descricao
+  , preco
+  , id_tipo_produto
+) VALUES (
+    'Impr. Laser'
+   , 500
+   , 2
+);
