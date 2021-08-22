2a413724556a558031bde29b0959e0173ce41b2f
romulovieira777 <romulo.vieira777@gmail.com>
2021-08-22 11:33:27 -0300

    Criando Aplicações CRUD com Java, Python e DB

--- Seção 18 - Criando Aplicações CRUD com Java, Python e Banco de Dados/JavaBD/src/jmongodb/Utils.java
+++ Seção 18 - Criando Aplicações CRUD com Java, Python e Banco de Dados/JavaBD/src/jmongodb/Utils.java
@@ -5,6 +5,7 @@ import java.util.Scanner;
 
 import org.bson.Document;
 import org.bson.conversions.Bson;
+import org.bson.types.ObjectId;
 import org.json.JSONObject;
 
 import com.mongodb.MongoClientSettings;
@@ -14,136 +15,138 @@ import com.mongodb.client.MongoClients;
 import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoCursor;
 import com.mongodb.client.MongoDatabase;
+import com.mongodb.client.result.DeleteResult;
+import com.mongodb.client.result.UpdateResult;
 
 import static com.mongodb.client.model.Updates.combine;
 import static com.mongodb.client.model.Updates.set;
 
 
-public class Utils {
+ublic class Utils {
 	
 	static Scanner teclado = new Scanner(System.in);
-
+	
 	public static MongoCollection<Document> conectar() {
 		try {
 			MongoClient conn = MongoClients.create(
 				MongoClientSettings.builder()
-				.applyToClusterSettings(builder ->
-				  builder.hosts(Arrays.asList(new ServerAddress("localhost", 27017))))
-				       .build());
-
-			MongoDatabase database = conn.getDatabase("nome do banco de dados");
-			MongoCollection<Document> collection = database.getCollection("produtos"); // nome da coleção
-
+					.applyToClusterSettings(builder ->
+					  builder.hosts(Arrays.asList(new ServerAddress("localhost", 27017))))
+							.build());
+			
+			MongoDatabase database = conn.getDatabase("jmongo");
+			MongoCollection<Document> collection = database.getCollection("produtos");
+			
 			return collection;
 		}catch(Exception e) {
 			e.printStackTrace();
-			return null
+			return null;
 		}
 	}
-
+	
 	public static void desconectar(MongoCursor<Document> cursor) {
 		cursor.close();
 	}
 	
 	public static void listar() {
 		MongoCollection<Document> collection = conectar();
-
-		if(collection.countDocument() > 0) {
+		
+		if(collection.countDocuments() > 0) {
 			MongoCursor<Document> cursor = collection.find().iterator();
-
+			
 			try {
-				System.out.println("Listando produtos...");
+				System.out.println("Listando Produtos...");
 				System.out.println("--------------------");
 				while(cursor.hasNext()) {
-					String json = cursor.next().toJson();					
-
-					JSONObject obj = new = JSONObject(json);
+					String json = cursor.next().toJson();
+					
+					JSONObject obj = new JSONObject(json);
 					JSONObject id = obj.getJSONObject("_id");
-
+					
 					System.out.println("ID: " + id.get("$oid"));
 					System.out.println("Produto: " + obj.get("nome"));
 					System.out.println("Preço: " + obj.get("preco"));
 					System.out.println("Estoque: " + obj.get("estoque"));
 					System.out.println("----------------------");
+					
 				}
-			} catch(Exception e) {
+			}catch(Exception e) {
 				e.printStackTrace();
 			}
 			desconectar(cursor);
-		} else {
-			System.out.println("Não existem documentos cadastrados.")
+		}else {
+			System.out.println("Não existem documentos cadastrados.");
 		}
 	}
 	
 	public static void inserir() {
 		MongoCollection<Document> collection = conectar();
-
+		
 		System.out.println("Informe o nome do produto: ");
 		String nome = teclado.nextLine();
-
+		
 		System.out.println("Informe o preço do produto: ");
-		String preco = teclado.nextFloat();
-
+		float preco = teclado.nextFloat();
+		
 		System.out.println("Informe o estoque: ");
 		int estoque = teclado.nextInt();
-
+		
 		JSONObject nproduto = new JSONObject();
 		nproduto.put("nome", nome);
 		nproduto.put("preco", preco);
 		nproduto.put("estoque", estoque);
-
-		collection.insertOne(Document.parse(nproduto.toString()));
-
-		System.out.println("O produto " + nome + " foi inserido com sucesso.");
 		
+		collection.insertOne(Document.parse(nproduto.toString()));
+		
+		System.out.println("O produto " + nome + " foi inserido com sucesso.");
 	}
 	
 	public static void atualizar() {
 		MongoCollection<Document> collection = conectar();
-
+		
 		System.out.println("Informe o ID do produto: ");
 		String _id = teclado.nextLine();
-
+		
 		System.out.println("Informe o nome do produto: ");
 		String nome = teclado.nextLine();
-
+		
 		System.out.println("Informe o preço: ");
 		float preco = teclado.nextFloat();
-
+		
 		System.out.println("Informe o estoque: ");
 		int estoque = teclado.nextInt();
-
+		
+		Bson query = combine(set("nome", nome), set("preco", preco), set("estoque", estoque));
+		
 		try {
-			Bson query = combine(set("nome", nome), set("preco", preco), set("estoque", estoque));
-
 			UpdateResult res = collection.updateOne(new Document("_id", new ObjectId(_id)), query);
-
+			
 			if(res.getModifiedCount() == 1) {
 				System.out.println("O produto " + nome + " foi atualizado com sucesso.");
-			} else {
-				System.out.println("O produto não foi atualizado.");
+			}else {
+				System.out.println("O produto não pode ser atualizado.");
 			}
-		} catch(java.lang.IllegalArgumentException e) {
+		}catch(java.lang.IllegalArgumentException e) {
 			System.out.println("ID inválido...");
 		}
 	}
 	
 	public static void deletar() {
-		MongoClient>Document> collection = conectar();
-
-		System.out.println("informe o ID do produto: ");
+		MongoCollection<Document> collection = conectar();
+		
+		System.out.println("Informe o ID do produto: ");
 		String _id = teclado.nextLine();
-
+		
 		try {
 			DeleteResult res = collection.deleteOne(new Document("_id", new ObjectId(_id)));
-
-			if(res.getDeleteCount() == 1) {
+			
+			if(res.getDeletedCount() == 1) {
 				System.out.println("O produto foi excluído com sucesso.");
-			} else {
+			}else {
 				System.out.println("Não foi possível excluir o produto.");
 			}
-		} catch(java.lang.IllegalArgumentException e) {
-			System.out.println("ID inválido..."); 
+		}catch(java.lang.IllegalArgumentException e) {
+			System.out.println("ID inválido...");
 		}
 	}
 	
--- Seção 18 - Criando Aplicações CRUD com Java, Python e Banco de Dados/JavaBD/src/jmysql/Utils.java
+++ Seção 18 - Criando Aplicações CRUD com Java, Python e Banco de Dados/JavaBD/src/jmysql/Utils.java
@@ -10,27 +10,27 @@ import java.util.Scanner;
 public class Utils {
 	
 	static Scanner teclado = new Scanner(System.in);
-
+	
 	public static Connection conectar() {
 		String CLASSE_DRIVER = "com.mysql.jdbc.Driver";
-		String USUARIO = "";
-		String SENHA = "";
+		String USUARIO = "geek";
+		String SENHA = "university";
 		String URL_SERVIDOR = "jdbc:mysql://localhost:3306/jmysql?useSSL=false";
-
+		
 		try {
 			Class.forName(CLASSE_DRIVER);
 			return DriverManager.getConnection(URL_SERVIDOR, USUARIO, SENHA);
-		} catch(Exception e) {
+		}catch(Exception e) {
 			if(e instanceof ClassNotFoundException) {
 				System.out.println("Verifique o driver de conexão.");
-			} else {
+			}else {
 				System.out.println("Verifique se o servidor está ativo.");
 			}
 			System.exit(-42);
 			return null;
 		}
 	}
-
+	
 	public static void desconectar(Connection conn) {
 		if(conn != null) {
 			try {
@@ -39,26 +39,25 @@ public class Utils {
 				System.out.println("Não foi possível fechar a conexão.");
 				e.printStackTrace();
 			}
-
 		}
 	}
 	
 	public static void listar() {
 		String BUSCAR_TODOS = "SELECT * FROM produtos";
-
+		
 		try {
 			Connection conn = conectar();
 			PreparedStatement produtos = conn.prepareStatement(BUSCAR_TODOS);
 			ResultSet res = produtos.executeQuery();
-
+			
 			res.last();
 			int qtd = res.getRow();
 			res.beforeFirst();
-
+			
 			if(qtd > 0) {
 				System.out.println("Listando produtos...");
-				System.out.println("--------------------");
-
+				System.out.println("---------------------");
+				
 				while(res.next()) {
 					System.out.println("ID: " + res.getInt(1));
 					System.out.println("Produto: " + res.getString(2));
@@ -66,129 +65,128 @@ public class Utils {
 					System.out.println("Estoque: " + res.getInt(4));
 					System.out.println("--------------------");
 				}
-			} else {
+			}else {
 				System.out.println("Não existem produtos cadastrados.");
 			}
 			produtos.close();
 			desconectar(conn);
-		} catch(Exception e) {
+		}catch(Exception e) {
 			e.printStackTrace();
 			System.err.println("Erro buscando produtos.");
-			System.exit(-42);;
+			System.exit(-42);
 		}
 	}
 	
 	public static void inserir() {
 		System.out.println("Informe o nome do produto: ");
 		String nome = teclado.nextLine();
-
+		
 		System.out.println("Informe o preço do produto: ");
 		float preco = teclado.nextFloat();
-
+		
 		System.out.println("Informe a quantidade em estoque: ");
 		int estoque = teclado.nextInt();
-
-		String INSERIR = "INSERT INTO produtos (nomes, preco, estoque) VALUES (?, ?, ?)";
-
+		
+		String INSERIR = "INSERT INTO produtos (nome, preco, estoque) VALUES (?, ?, ?)";
+		//SQL Injection
+		
 		try {
 			Connection conn = conectar();
 			PreparedStatement salvar = conn.prepareStatement(INSERIR);
-
+			
 			salvar.setString(1, nome);
-			salvar.setFloat(2, preco);
-			salvar.setInt(3, estoque);
-
+			salvar.setFloat(2,  preco);
+			salvar.setInt(3,  estoque);
+			
 			salvar.executeUpdate();
 			salvar.close();
 			desconectar(conn);
-
 			System.out.println("O produto " + nome + " foi inserido com sucesso.");
-		} catch(Exception e) {
+		}catch(Exception e) {
 			e.printStackTrace();
 			System.err.println("Erro salvando produto");
 			System.exit(-42);
-
 		}
 	}
 	
 	public static void atualizar() {
 		System.out.println("Informe o código do produto: ");
 		int id = Integer.parseInt(teclado.nextLine());
-
+		
 		String BUSCAR_POR_ID = "SELECT * FROM produtos WHERE id=?";
-
+		
 		try {
 			Connection conn = conectar();
 			PreparedStatement produto = conn.prepareStatement(BUSCAR_POR_ID);
 			produto.setInt(1, id);
 			ResultSet res = produto.executeQuery();
-
+			
 			res.last();
 			int qtd = res.getRow();
 			res.beforeFirst();
-
+			
 			if(qtd > 0) {
 				System.out.println("Informe o nome do produto: ");
 				String nome = teclado.nextLine();
-
+				
 				System.out.println("Informe o preço do produto: ");
 				float preco = teclado.nextFloat();
-
+				
 				System.out.println("Informe a quantidade em estoque: ");
 				int estoque = teclado.nextInt();
-
-				String ATUALIZAR = "UPDATE produto SET nome=?, preco=?, estoque=? WHERE id=?";
+				
+				String ATUALIZAR = "UPDATE produtos SET nome=?, preco=?, estoque=? WHERE id=?";
 				PreparedStatement upd = conn.prepareStatement(ATUALIZAR);
-
-				upd.setString(1, nome);
-				upd.setFloat(2, preco);
-				upd.setInt(3, estoque);
-				upd.setInt(4, id);
-
+				
+				upd.setString(1,  nome);
+				upd.setFloat(2,  preco);
+				upd.setInt(3,  estoque);
+				upd.setInt(4,  id);
+				
 				upd.executeUpdate();
 				upd.close();
 				desconectar(conn);
 				System.out.println("O produto " + nome + " foi atualizado com sucesso.");
-
-			} else {
+			}else {
 				System.out.println("Não existe produto com o id informado.");
 			}
-
-		} catch(Exception e) {
+			
+		}catch(Exception e) {
 			e.printStackTrace();
 			System.err.println("Erro ao atualizar produto.");
 			System.exit(-42);
 		}
+		
 	}
 	
 	public static void deletar() {
-		String DELETAR = "DELETE FROM produtos WHERE id=?";
+		String DELETAR = "DELETE FROM produtos WHERE  id=?";
 		String BUSCAR_POR_ID = "SELECT * FROM produtos WHERE id=?";
-
+		
 		System.out.println("Informe o código do produto: ");
 		int id = Integer.parseInt(teclado.nextLine());
-
+		
 		try {
 			Connection conn = conectar();
 			PreparedStatement produto = conn.prepareStatement(BUSCAR_POR_ID);
 			produto.setInt(1, id);
 			ResultSet res = produto.executeQuery();
-
+			
 			res.last();
 			int qtd = res.getRow();
-
+			res.beforeFirst();
+			
 			if(qtd > 0) {
 				PreparedStatement del = conn.prepareStatement(DELETAR);
 				del.setInt(1, id);
 				del.executeUpdate();
 				del.close();
 				desconectar(conn);
-				System.out.print("O produto foi deletado com sucesso.");
+				System.out.println("O produto foi deletado com sucesso.");
 			}else {
-				System.out.println("Não existe o produto com o id informado.s");
+				System.out.println("Não existe o produto com o id informado.");
 			}
-
-		} catch(Exception e) {
+		}catch(Exception e) {
 			e.printStackTrace();
 			System.err.println("Erro ao deletar produto.");
 			System.exit(-42);
